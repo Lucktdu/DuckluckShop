5b9127b2391e18455b7adc9faa18b0635bc8efaf
LUCAS VALIM <116410099+Lucktdu@users.noreply.github.com>
2022-12-03 13:46:56 -0400

    Comitada monstra

--- Script.js
+++ Script.js
@@ -1,106 +1,68 @@
+let carrinho = [];
 
-
-
+function attcarrinho() {
+  let divcarrinho = document.getElementById("carrinho");
+}
 
 async function shoplucao() {
   let resultado = await fetch(
     "https://api.mercadolibre.com/sites/MLB/search?q=Iphone14"
   );
-
   let boalucao = await resultado.json();
 
-  for (i = 0; i <= 1; i++) {
-  
+  let resul = boalucao.results;
+  let res = resul;
 
-    let titulo = document.createElement("h1");
-    let preco = document.createElement("h2");
-    let img = document.createElement("img");
+  for (i = 0; i < res.length; i++) {
+    // Titulo Produtos
+    let divprodutos = document.getElementById("produtos");
+    let titulo = document.createElement("div");
+    titulo.className = "produto";
+    titulo.innerHTML = res[i].title;
+
+    // Valor dos produtos
+    let valor = document.createElement("div");
+    valor.className = "valor";
+    valor.innerHTML = res[i].price;
+
+    // imagem produtos
+    let imagem = document.createElement("img");
+    imagem.src = res[i].thumbnail;
+    imagem.className = "Imgphone";
+    let divimagem = document.createElement("div");
+    divimagem.append(imagem);
+    document.getElementsByClassName("produto").innerHTML = imagem[i];
+
+    // Botão Carrinho ---------
     let botao = document.createElement("input");
-    let div = document.createElement("div");
-    let quantidade = document.createElement("span");
-    let divc =  document.getElementById('carrinho')
-    
-    // body > section > Div Gato
-    pegarbody = document.getElementById("gato");
-    pegarcarrinho = document.getElementById("carrinho");
-    // Botao do menu de compra
     botao.type = "button";
-    botao.value = "Comprar";
-    botao.id = boalucao.results[i].id;
-    
-    // Recebendo informações do array results
-    titulo.innerText = boalucao.results[i].title;
-    let titulo2 = boalucao.results[i].title;
-    preco.innerText = boalucao.results[i].price;
-    let preco2 = boalucao.results[i].price;
-    
-    img.src = boalucao.results[i].thumbnail;
-    let img2 = boalucao.results[i].thumbnail;
-    quantidade.innerText = boalucao.results[i].sold_quantity;
-    let quantidade2 = boalucao.results[i].sold_quantity;
+    botao.value = "Envia ao carrinho";
+    botao.className = "btc";
+    botao.id = res[i].id;
 
-    // Definindo classes para cada propriedade do site
-    titulo.className = "txt";
-    preco.className = "valor";
-    img.className = "foto";
-    botao.className = "btcomprar";
-    div.className = "produtos";
+    // console.log('produtos');
+    divprodutos.appendChild(titulo);
+    divprodutos.appendChild(valor);
+    divprodutos.appendChild(imagem);
+    divprodutos.appendChild(botao);
 
-    // Adicionar Itens no body e div
-    div.appendChild(botao);
-    div.appendChild(titulo);
-    div.appendChild(preco);
-    div.appendChild(img);
-    div.appendChild(quantidade);
+    let divbtc = document.getElementsByClassName("btc");
+    divbtc[i].addEventListener("click", function () {
+      var btcid = botao.getAttribute("id");
+      for (c = 0; c < res.length; c++) {
+        if (res[c].id === btcid) {
+          // console.log(res[c])
+          //Adicionar o produto a um array.
+          carrinho.push(res[c]);
+          console.log(carrinho);
+          // Chamar uma função que adiciona ao carrinho.
+        }
+      }
 
-    pegarbody.appendChild(div);
-
-
-  
-
- var links = document.getElementById(botao.id);
-
-
- links.addEventListener("click", function () {
-  let titulo = document.createElement("h1");
-  let preco = document.createElement("h2");
-  let img = document.createElement("img");
-  let quantidade = document.createElement("span");
-
-  
-
-   titulo.innerText = titulo2
-   preco.innerText = preco2
-   img.src = img2
-   quantidade.innerText = quantidade2
-  
-   
-
-
-   
-   
-   divc.appendChild(titulo);
-   divc.appendChild(preco);
-   divc.appendChild(img);
-   divc.appendChild(quantidade);
-
-   pegarbody.appendChild(divc);
- })
-}
-  
-
-
-
-
-
-
-
-
-    
-   
- 
+      //  res[btcid].filter(res.title)
+      //  document.getElementsByClassName('carrinho').innerHTML =
+    });
+  }
 }
 
-
-
-shoplucao()
+shoplucao();
