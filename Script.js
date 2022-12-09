6d7e52abcf84b9de6e1920f0b92db12325e0ae8b
LUCAS VALIM <116410099+Lucktdu@users.noreply.github.com>
2022-12-09 19:31:01 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,70 +10,69 @@ async function fetchByQuery(query = "Iphone14") {
     `https://api.mercadolibre.com/sites/MLB/search?q=${query}`
   );
   return (await result.json()).results;
-
-  function createProductElement(className) {
-    const ProductElement = document.createElement("div");
-    ProductElement.className = `${className}product `;
-    return ProductElement;
-  }
-
-  function createTitle(text, className) {
-    const titleElement = document.createElement("div");
-    titleElement.className = `${className}title`;
-    titleElement.innerText = text;
-    return titleElement;
-  }
-
-  function createValue(value, className) {
-    const valueElement = document.createElement("div");
-    valueElement.className = `${className} valor`;
-    valueElement.innerText = value;
-    return valueElement;
-  }
-
-  function createImage(thumbnail, className) {
-    const imageElement = document.createElement("img");
-    imageElement.src = thumbnail;
-    imageElement.className = `${className}Imgphone`;
-
-    const imageDiv = document.createElement("div");
-    imageDiv.appendChild(imageElement);
-    return imageDiv;
-  }
-
-  function createButton(id, className) {
-    const button = document.createElement("input");
-    button.type = "button";
-    button.value = "Enviar ao carrinho";
-    button.id = id;
-    button.className = `${className}btc`;
-    button.addEventListener("click", buttonCartClickHandler);
-    return button;
-  }
-
-  function buttonCartClickHandler(event) {
-    event.preventDefault();
-    for (c = 0; c < productsList.length; c++) {
-      if (productsList[c].id === event.target.id) {
-        cartList.push(productsList[c]);
-        break;
-      }
-    }
-
-    addItemToCar();
-  }
 }
-function createProductElement(
-  productsList,
+function createProductElement(className) {
+  const ProductElement = document.createElement("div");
+  ProductElement.className = `${className}product `;
+  return ProductElement;
+}
+
+function createTitle(text, className) {
+  const titleElement = document.createElement("div");
+  titleElement.className = `${className}title`;
+  titleElement.innerText = text;
+  return titleElement;
+}
+
+function createValue(value, className) {
+  const valueElement = document.createElement("div");
+  valueElement.className = `${className} valor`;
+  valueElement.innerText = value;
+  return valueElement;
+}
+
+function createImage(thumbnail, className) {
+  const imageElement = document.createElement("img");
+  imageElement.src = thumbnail;
+  imageElement.className = `${className}Imgphone`;
+
+  const imageDiv = document.createElement("div");
+  imageDiv.appendChild(imageElement);
+  return imageDiv;
+}
+
+function createButton(id, className) {
+  const button = document.createElement("input");
+  button.type = "button";
+  button.value = "Enviar ao carrinho";
+  button.id = id;
+  button.className = `${className}btc`;
+  button.addEventListener("click", buttonCartClickHandler);
+  return button;
+}
+
+function buttonCartClickHandler(event) {
+  event.preventDefault();
+  for (c = 0; c < productsList.length; c++) {
+    if (productsList[c].id === event.target.id) {
+      cartList.push(productsList[c]);
+      break;
+    }
+  }
+
+  addItemToCar();
+}
+function createProductElements(
+  productList,
   fatherElement,
-  classinitial = "home"
+  classInitial = "home"
 ) {
-  for (i = 0; i < productsList.length; i++) {
-    const productElement = createProductElement(classinitial);
-    const titleElement = createTitle(productsList[i].title, classinitial);
-    const valueElement = createVKalue(productsList[i].price, classinitial);
-    const imageDiv = createImage(productsList[i].thumbnail, classinitial);
-    const cartButton = createCartButton(productsList[i].id, classinitial);
+  for (i = 0; i < productList.length; i++) {
+    const productElement = createProductElements(classInitial);
+    const titleElement = createTitle(productList[i].title, classInitial);
+    const valueElement = createValue(productList[i].price, classInitial);
+    const imageDiv = createImage(productList[i].thumbnail, classInitial);
+    const cartButton = createCartButton(productList[i].id, classInitial);
 
     productElement.appendChild(titleElement);
     productElement.appendChild(valueElement);
@@ -86,12 +85,12 @@ async function createHome() {
   const productElement = document.getElementById("produtos");
   const products = await fetchByQuery();
   productsList = products;
-  createProductElement(products, productElement);
+  createProductElements(products, productElement);
 }
 
 function addItemToCar() {
   const cartElement = document.getElementById("carrinho");
   cartElement.innerText = "";
 
-  createProductElement(cartList, cartElement, "cart");
+  createProductElements(cartList, cartElement, "cart");
 }
